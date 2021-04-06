35314f4bc3343bbcf43e09c40da4d855583e88e1
Matan Shenhav <matan@fluxcraft.net>
2021-04-06 09:25:10 +0300

    Add space routing

---
+++ src/routes/s[id]/index.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+  export async function load({ page, fetch }) {
+    const { id } = page.params;
+    const space = await fetch(`s${id}.json`).then((r)=>r.json());
+
+    return {
+      props: { id, space }
+    }
+  }
+</script>
+
+<script>
+  export let id;
+  export let space;
+</script>
+
+<svelte:head>
+  <title>{space.name}</title>
+</svelte:head>
+
+<h1>{space.name}</h1>
+
+<p>{JSON.stringify(space)}</p>
