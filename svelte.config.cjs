674003df4164795a09b9f9381c79984fea5391c1
Matan Shenhav <matan@fluxcraft.net>
2021-04-14 14:18:01 +0300

    Revert to adapter node until Supabase source mapping bug is resolved

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -17,10 +17,10 @@ module.exports = {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		//adapter: node(),
+		adapter: node(),
 		
 		// non-default
-		adapter: netlify(),
+		//adapter: netlify(),
 		
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
