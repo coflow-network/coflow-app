27df0757c0703002fcc03c7a4e235a7d0ead7d04
Matan Shenhav <matan@fluxcraft.net>
2021-05-09 18:50:45 +0300

    Modify FlowController buttons to vertical layout

--- src/lib/feat/flows/FlowController.svelte
+++ src/lib/feat/flows/FlowController.svelte
@@ -19,14 +19,13 @@
     }
 </script>
 
-
 <div class="controls">
-    <BigButton action={prev} color="neutral" text="&#x276E;" />
-    <BigButton action={next} color="neutral" text="&#x276F;" />
+    <BigButton action={prev} color="neutral" text="&uarr" />
+    <BigButton action={next} color="neutral" text="&darr" />
 </div>
 
 <style>
     .controls {
-        @apply absolute z-10 bottom-2 left-6 right-6 h-32 flex flex-row justify-between;
+        @apply absolute z-10 bottom-4 right-4 w-16 h-32 flex flex-col justify-between;
     }
 </style>
